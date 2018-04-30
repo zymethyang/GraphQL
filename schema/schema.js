821e5dfbb361fdb6ff4ef59a7e981228e0db7322
Tri Nguyen <zymethyang@gmail.com>
2018-04-30 12:17:27 +0700

    finished root query

---
+++ schema/schema.js
@@ -0,0 +1,53 @@
+const graphql = require('graphql');
+const {
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLInt,
+    GraphQLSchema
+} = graphql;
+const _ = require('lodash');
+
+const users = [
+    {
+        id: '23', firstname: 'Bill', age: 20,
+        id: '47', firstname: 'Hi', age: 23,
+        id: '24', firstname: 'Ka', age: 21,
+    }
+]
+
+const UserType = new GraphQLObjectType({
+    name: 'User',
+    fields: {
+        id: {
+            type: GraphQLString
+        },
+        firstname: {
+            type: GraphQLString
+        },
+        age: {
+            type: GraphQLInt
+        }
+    }
+});
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        user: {
+            type: UserType,
+            args: {
+                id: {
+                    type: GraphQLString
+                }
+            },
+            resolve(parentValue, args) {
+                return _.find(users, { id: args.id });
+            }
+        }
+    }
+});
+
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
+})
